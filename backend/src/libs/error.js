7118712c613b3796995cd7c78a60a6d6754ee32f
apisitprompha <apisitprompha@gmail.com>
2019-12-09 13:22:35 +0700

    delete node_modules

---
+++ backend/src/libs/error.js
@@ -0,0 +1,7 @@
+module.exports = {
+    server: 'server error',
+    params: 'params is not match',
+    email: 'email already exit',
+    photo: 'invalid picture',
+    token: 'invalid token'
+}
---
+++ backend/src/libs/imageupload.js
@@ -0,0 +1,24 @@
+ 
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+    destination: ('./public/images/'),
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + '-' + new Date().getTime().toString() + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 2 * 1024 * 1024 // 2MB
+    },
+    fileFilter: function(req, file, cb) {        
+        const ext = path.extname(file.originalname);
+        if(ext !== '.png' && ext !== '.jpg' && ext !== '.gif' && ext !== '.jpeg') {
+            return cb(new Error('On ly images are allowed'));
+        }
+        cb(null, true);
+    }
+}).array('picture', 10);
